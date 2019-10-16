0d7f58f8685b8955b934c2cda222d0b282accafe
Nick Saxton <nsaxton@Nicks-MacBook-Air.local>
2019-10-16 17:48:28 -0400

    Updates

---
+++ precache-manifest.75060dbc9a9f4375fff308b5f04d479c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d7dcd2beddac2cf9b3e6e3c16585b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ed41617f1c4617327c9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a19311967b787e62bc78",
+    "url": "/static/js/2.0d0a2893.chunk.js"
+  },
+  {
+    "revision": "9ed41617f1c4617327c9",
+    "url": "/static/js/main.e0ad4695.chunk.js"
+  },
+  {
+    "revision": "445609c0d6601aeb162b",
+    "url": "/static/js/runtime-main.9973fb62.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
